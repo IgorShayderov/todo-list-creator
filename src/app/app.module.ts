41a17575eef72f824f557121d97f5afe3ee027c7
IgorShayderov <flashmob123@gmail.com>
2021-01-21 23:24:33 +0200

    deployed angular application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProjectComponent } from './project/project.component';
+import { TodoCreateFormComponent } from './todo-create-form/todo-create-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectComponent,
+    TodoCreateFormComponent
   ],
   imports: [
     BrowserModule
